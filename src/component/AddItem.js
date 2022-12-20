1a9827b2ee9b826566d47216367fa1500bd6450e
Sumitray1 <sumitray206@gmail.com>
2022-12-20 11:38:06 +0545

    lettle modified

--- src/component/AddItem.js
+++ src/component/AddItem.js
@@ -1,9 +1,17 @@
 import React from "react";
 import "../component/addItem.css";
+// import Button from "@mui/material/Button";
+// import DeleteIcon from "@mui/material/Button";
+
 const AddItem = (prop) => {
   return (
     <div className="delete-Item">
-      <button onClick={() => prop.onSelect(prop.id)}>X</button>
+      {/* <Button variant="outlined" startIcon={<DeleteIcon />}>
+        Delete
+      </Button> */}
+      <button onClick={() => prop.onSelect(prop.id)} className="close-button">
+        X
+      </button>
       <div div> {prop.text}</div>
     </div>
   );
